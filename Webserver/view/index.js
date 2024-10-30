61c11fff9bbb2a2c9fca0b28802a9cace4c4a211
tuanthanhhh <letuanthanh.work@gmail.com>
2024-10-31 00:37:25 +0700

    init webserver

---
+++ Webserver/view/index.js
@@ -0,0 +1,20 @@
+function showDateTime() {
+    const now = new Date();
+    const daysOfWeek = ["Chủ Nhật", "Thứ Hai", "Thứ Ba", "Thứ Tư", "Thứ Năm", "Thứ Sáu", "Thứ Bảy"];
+    const dayOfWeek = daysOfWeek[now.getDay()]; // Lấy thứ hiện tại
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Tháng tính từ 0
+    const year = now.getFullYear();
+
+    const formattedDateTime = `${dayOfWeek}, ngày ${day} tháng ${month} năm ${year}`;
+    document.getElementById("datetime").innerText = formattedDateTime;
+}
+
+// Gọi hàm hiển thị thời gian ban đầu và cập nhật mỗi giây
+showDateTime();
+setInterval(showDateTime, 1000);
+
+
+// script.js
+
+
